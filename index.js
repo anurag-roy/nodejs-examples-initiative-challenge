c326baae46ef3353648c8f67c6bf6a12fb35cc38
Anurag Roy <anurag@2pirad.com>
2020-10-28 03:47:49 +0530

    Added tests

--- index.js
+++ index.js
@@ -14,57 +14,58 @@ app.set("view engine", "hbs");
 
 /** Route i */
 app.get("/dependencies", (_request, response) => {
-  response.render("dependencies", { dependencies });
+	// Pass the dependencies object to the hbs template
+	response.render("dependencies", { dependencies });
 });
 
 /** Route ii */
 app.get("/minimum-secure", async (_request, response) => {
-  const releasesArray = await getJson(ENDPOINT);
+	const releasesArray = await getJson(ENDPOINT);
 
-  // Get Minimum Secure Releases Array
-  const minimumSecureReleases = getHighestVersions(
-    releasesArray.filter((release) => release.security),
-  );
+	// Get Minimum Secure Releases Array
+	const minimumSecureReleases = getHighestVersions(
+		releasesArray.filter((release) => release.security),
+	);
 
-  response.json(minimumSecureReleases);
+	response.json(minimumSecureReleases);
 });
 
 /** Route iii */
 app.get("/latest-releases", async (_request, response) => {
-  const releasesArray = await getJson(ENDPOINT);
+	const releasesArray = await getJson(ENDPOINT);
 
-  // Get Latest Releases Array
-  const latestReleases = getHighestVersions(releasesArray);
+	// Get Latest Releases Array
+	const latestReleases = getHighestVersions(releasesArray);
 
-  response.json(latestReleases);
+	response.json(latestReleases);
 });
 
 /** Get highest version of each release line from the passed array of releases */
 const getHighestVersions = (releasesArray) => {
-  const highestVersionMap = new Map();
+	const highestVersionMap = new Map();
 
-  releasesArray.forEach((release) => {
-    // Get major version and create a key, e.g. v14, v8, etc.
-    const majorVersionNo = semver.major(release.version);
-    const key = `v${majorVersionNo}`;
+	releasesArray.forEach((release) => {
+		// Get major version and create a key, e.g. v14, v8, etc.
+		const majorVersionNo = semver.major(release.version);
+		const key = `v${majorVersionNo}`;
 
-    // If no entry is present for a version, create a new one
-    if (!highestVersionMap.has(key)) {
-      highestVersionMap.set(key, release);
-    } else {
-      // Else retrieve the stored entry and compare
-      // If this release version is greater, replace with this release
-      const storedObject = highestVersionMap.get(key);
-      if (semver.gt(release.version, storedObject.version)) {
-        highestVersionMap.set(key, release);
-      }
-    }
-  });
+		// If no entry is present for a version, create a new one
+		if (!highestVersionMap.has(key)) {
+			highestVersionMap.set(key, release);
+		} else {
+			// Else retrieve the stored entry and compare
+			// If this release version is greater, replace with this release
+			const storedObject = highestVersionMap.get(key);
+			if (semver.gt(release.version, storedObject.version)) {
+				highestVersionMap.set(key, release);
+			}
+		}
+	});
 
-  // Create and return the map as an object
-  return Object.fromEntries(highestVersionMap);
+	// Create and return the map as an object
+	return Object.fromEntries(highestVersionMap);
 };
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });
--- test/index.js
+++ test/index.js
@@ -1,33 +1,104 @@
-const tape = require('tape')
-const bent = require('bent')
-const getPort = require('get-port')
+const tape = require("tape");
+const bent = require("bent");
+const nock = require("nock");
 
-const server = require('../')
+const getJSON = bent("json");
+const getBuffer = bent("buffer");
 
-const getJSON = bent('json')
-const getBuffer = bent('buffer')
+const server = require("../");
 
-// Use `nock` to prevent live calls to remote services
+const nockResponseArray = [
+	{
+		version: "v15.0.1",
+		date: "2020-10-21",
+		files: [],
+		npm: "7.0.3",
+		v8: "8.6.395.17",
+		uv: "1.40.0",
+		zlib: "1.2.11",
+		openssl: "1.1.1g",
+		modules: "88",
+		lts: false,
+		security: false,
+	},
+	{
+		version: "v15.0.0",
+		date: "2020-10-20",
+		files: [],
+		npm: "7.0.2",
+		v8: "8.6.395.16",
+		uv: "1.40.0",
+		zlib: "1.2.11",
+		openssl: "1.1.1g",
+		modules: "88",
+		lts: false,
+		security: true,
+	},
+	{
+		version: "v14.15.0",
+		date: "2020-10-27",
+		files: [],
+		npm: "6.14.8",
+		v8: "8.4.371.19",
+		uv: "1.40.0",
+		zlib: "1.2.11",
+		openssl: "1.1.1g",
+		modules: "83",
+		lts: "Fermium",
+		security: true,
+	},
+	{
+		version: "v14.14.0",
+		date: "2020-10-15",
+		files: [],
+		npm: "6.14.8",
+		v8: "8.4.371.19",
+		uv: "1.40.0",
+		zlib: "1.2.11",
+		openssl: "1.1.1g",
+		modules: "83",
+		lts: false,
+		security: true,
+	},
+];
 
-const context = {}
+const scope = nock("https://nodejs.org").get("/dist/index.json").reply(200, nockResponseArray);
+scope.persist(true);
 
-tape('setup', async function (t) {
-	const port = await getPort()
-	context.server = server.listen(port)
-	context.origin = `http://localhost:${port}`
+const context = {};
 
-	t.end()
-})
+tape("setup", async function (t) {
+	context.origin = `http://localhost:3000`;
+	t.end();
+});
 
-tape('should get dependencies', async function (t) {
-	const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
+tape("should get dependencies", async function (t) {
+	t.plan(4);
 
-	// assertions etc
-})
+	const html = (await getBuffer(`${context.origin}/dependencies`)).toString();
 
-// more tests
+	t.match(html, /.*bent.*/, "should contain bent");
+	t.match(html, /.*express.*/, "should contain express");
+	t.match(html, /.*hbs.*/, "should contain hbs");
+	t.match(html, /.*semver.*/, "should contain semver");
+});
 
-tape('teardown', function (t) {
-	context.server.close()
-	t.end()
-})
+tape("should get minimum secure versions", async function (t) {
+	t.plan(2);
+
+	const response = await getJSON(`${context.origin}/minimum-secure`);
+
+	t.deepEqual(response.v15, nockResponseArray[1], "v15 minimum secure version should match");
+	t.deepEqual(response.v14, nockResponseArray[2], "v14 minimum secure version should match");
+});
+
+tape("should get latest-releases", async function (t) {
+	t.plan(2);
+
+	const response = await getJSON(`${context.origin}/latest-releases`);
+
+	t.deepEqual(response.v15, nockResponseArray[0], "v15 latest release version should match");
+	t.deepEqual(response.v14, nockResponseArray[2], "v14 latest release version should match");
+});
+
+tape.onFinish(() => process.exit(0));
